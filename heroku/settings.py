205c442a6b43e0b8b2a85e9be2928be7ccdee7c0
edwin muliro <edwinmuliro67@gmail.com>
2021-04-05 01:01:40 +0300

    just heroku

--- heroku/settings.py
+++ heroku/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polo',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
--- polo/views.py
+++ polo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from gunicorn import util
 
 def index(request):
     return render(request,'index.html')
